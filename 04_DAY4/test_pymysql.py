20731b7eed8586bb1eb5e186e2dfeccf059a3fc3
jun2k5 <jun2k5@naver.com>
2025-12-05 23:49:42 +0900

    오즈코딩 DB SQL 5일차 과제 및 팀프로젝트

--- 04_DAY4/test_pymysql.py
+++ 04_DAY4/test_pymysql.py
@@ -11,23 +11,29 @@ connection = pymysql.connect(
     host='127.0.0.1',
     user='root',
     password = DB_PWD,
-    database = 'testdatabase',
+    database = 'classicmodels',
     charset = 'utf8mb4',
     cursorclass = pymysql.cursors.DictCursor
 )
 
 cursor = connection.cursor()
 
-sql = "SELECT * FROM pets"
+#데이터 조회 SELECT
+sql = "SELECT * FROM customers"
 cursor.execute(sql)
 
-customers = cursor.fetchall()
-
-
+customers = cursor.fetchone()
+print("pets:", customers['customerNumber'])
+print("pets:", customers['customerName'])
+print("pets:", customers['country'])
 
 cursor.close()
 connection.close()
 
+#데이터 입력 INSERT
+
+# sql = "INSERT INTO pets (name, species, breed) VALUES (%s, %s, %s)"
+# values = ("Buddy", "Dog", "Golden Retriever")
 
 
 
